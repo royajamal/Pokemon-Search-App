6b76d92da8dd4e9f14b6096f7f20bb2cd6006f4f
unknown <royajamal2003@gmail.com>
2024-07-10 06:36:10 -0700

    script.js linters error solved

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('search-button').addEventListener('click', async () => {
-  const searchInput = document.getElementById("search-input").value.trim().toLowerCase();
+  const searchInput = document.getElementById('search-input').value.trim().toLowerCase();
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${searchInput}`;
   try {
     const response = await fetch(apiUrl);
@@ -16,33 +16,34 @@ document.getElementById('search-button').addEventListener('click', async () => {
     document.getElementById('pokemon-id').innerText = `#${data.id}`;
     document.getElementById('weight').innerText = `Weight: ${data.weight}`;
     document.getElementById('height').innerText = `Height: ${data.height}`;
-        // Add sprite image
+
+    // Add sprite image
     const spriteImg = document.createElement('img');
     spriteImg.id = 'sprite';
     spriteImg.src = data.sprites.front_default;
     document.getElementById('sprite-img').appendChild(spriteImg);
-    
+
     // Add Pokémon types
     const typesElement = document.getElementById('types');
-    data.types.forEach(type => {
+    data.types.forEach((type) => {
       const typeElement = document.createElement('span');
       typeElement.innerText = type.type.name.toUpperCase();
       typesElement.appendChild(typeElement);
-  });
-  // Update stats
-    document.getElementById('hp').innerText = data.stats.find(stat => stat.stat.name === 'hp').base_stat;
-    document.getElementById('attack').innerText = data.stats.find(stat => stat.stat.name === 'attack').base_stat;
-    document.getElementById('defense').innerText = data.stats.find(stat => stat.stat.name === 'defense').base_stat;
-    document.getElementById('special-attack').innerText = data.stats.find(stat => stat.stat.name === 'special-attack').base_stat;
-    document.getElementById('special-defense').innerText = data.stats.find(stat => stat.stat.name === 'special-defense').base_stat;
-    document.getElementById('speed').innerText = data.stats.find(stat => stat.stat.name === 'speed').base_stat;
-    
+    });
+
+    // Update stats
+    document.getElementById('hp').innerText = data.stats.find((stat) => stat.stat.name === 'hp').base_stat;
+    document.getElementById('attack').innerText = data.stats.find((stat) => stat.stat.name === 'attack').base_stat;
+    document.getElementById('defense').innerText = data.stats.find((stat) => stat.stat.name === 'defense').base_stat;
+    document.getElementById('special-attack').innerText = data.stats.find((stat) => stat.stat.name === 'special-attack').base_stat;
+    document.getElementById('special-defense').innerText = data.stats.find((stat) => stat.stat.name === 'special-defense').base_stat;
+    document.getElementById('speed').innerText = data.stats.find((stat) => stat.stat.name === 'speed').base_stat;
   } catch (error) {
     alert('Pokémon not found');
     clearPokemonData();
   }
 });
-    
+
 function clearPokemonData() {
   document.getElementById('pokemon-name').innerText = '';
   document.getElementById('pokemon-id').innerText = '';
